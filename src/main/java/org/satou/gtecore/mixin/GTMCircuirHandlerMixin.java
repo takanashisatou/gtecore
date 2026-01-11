8d4d376278549c69ba1cd6669064f5e06a492a9b
takanashisatou <156740808+takanashisatou@users.noreply.github.com>
2026-01-11 19:45:48 +0800

    Update gtecore 2.2.0 开发完毕

---
+++ src/main/java/org/satou/gtecore/mixin/GTMCircuirHandlerMixin.java
@@ -0,0 +1,37 @@
+package org.satou.gtecore.mixin;
+
+import com.gregtechceu.gtceu.api.capability.recipe.IO;
+import com.gregtechceu.gtceu.api.capability.recipe.ItemRecipeCapability;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
+import com.gregtechceu.gtceu.api.machine.WorkableTieredMachine;
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
+
+import com.gregtechceu.gtceu.common.data.GTItems;
+import com.hepdd.gtmthings.api.machine.trait.ProgrammableCircuitHandler;
+import com.hepdd.gtmthings.data.CustomItems;
+import it.unimi.dsi.fastutil.ints.Int2IntFunction;
+import org.jetbrains.annotations.NotNull;
+import org.satou.gtecore.common.ProgrammableCircuitHandlerModify;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = SimpleTieredMachine.class,priority = 2000)
+public class GTMCircuirHandlerMixin extends WorkableTieredMachine {
+
+    private GTMCircuirHandlerMixin(IMachineBlockEntity holder, int tier, Int2IntFunction tankScalingFunction, Object... args) {
+        super(holder, tier, tankScalingFunction, args);
+    }
+
+    @Inject(method = "createCircuitItemHandler", at = @At("HEAD"), remap = false, cancellable = true)
+    protected void createCircuitItemHandler(Object[] args, CallbackInfoReturnable<NotifiableItemStackHandler> cir) {
+        cir.setReturnValue(new ProgrammableCircuitHandlerModify(this));
+    }
+
+    @Override
+    protected @NotNull NotifiableItemStackHandler createImportItemHandler(Object @NotNull... args) {
+        return new NotifiableItemStackHandler(this, getRecipeType().getMaxInputs(ItemRecipeCapability.CAP), IO.IN).setFilter(i -> ((!i.is(CustomItems.VIRTUAL_ITEM_PROVIDER.get()) || !i.hasTag()) && !i.is(GTItems.PROGRAMMED_CIRCUIT.get())));
+    }
+}
